2bd7280d94b9ff74f08f888c2aa1954d909af78b
Catalin <catalinboruga@yahoo.com>
2015-06-06 21:02:48 +0200

    Add controller and session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
+  root 'movies#index'
+  resource :session
+  get 'signin' => 'sessions#new'
   get 'signup' => 'users#new'
   resources :users
-  root 'movies#index'
   resources :movies do 
     resources :reviews
   end
