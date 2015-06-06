5d4a80c5e48d9979f2c39219c1cb7b30f1f0b6fb
Catalin <catalinboruga@yahoo.com>
2015-06-06 21:53:22 +0200

    Add conditionals for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    session[:user_id] = @user.id
     if @user.save
       redirect_to @user, notice: "Thanks for signing up"
     else
