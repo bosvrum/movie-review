bed6a6047440fbe73fd4ba1ac206142dd6284672
Catalin <catalinboruga@yahoo.com>
2015-06-07 14:31:11 +0200

    Add destroy action for sign-out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    session[:user_id] = @user.id
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Thanks for signing up"
     else
       render :new
@@ -37,9 +37,9 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    if @user.destroy
-      redirect_to root_url, alert: "Account successfully deleted!"
-    end
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_url, alert: "Account successfully deleted!"
   end
 
   private
