5fb84f3abf41f215855817c07bcb249cd89b5a76
Catalin <catalinboruga@yahoo.com>
2015-06-10 21:14:24 +0200

    Add review join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610160651) do
+ActiveRecord::Schema.define(version: 20150610182320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20150610160651) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string   "name"
     t.integer  "stars"
     t.text     "comment"
     t.integer  "movie_id"
@@ -39,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150610160651) do
     t.string   "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
