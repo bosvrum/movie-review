75c1a23c2d2d5c5008fb615812fa9a7a374a4c15
Catalin <catalinboruga@yahoo.com>
2015-06-10 18:25:40 +0200

    Add admin to users and set authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606172517) do
+ActiveRecord::Schema.define(version: 20150610160651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,10 @@ ActiveRecord::Schema.define(version: 20150606172517) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "username"
+    t.boolean  "admin",           default: false
   end
 
 end
